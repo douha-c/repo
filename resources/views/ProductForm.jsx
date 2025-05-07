7c6ca3a572705f4f62054a8a335683813d62412f
douha-c <dev204douha@gmail.com>
2025-05-07 12:04:15 +0100

    Update ProductForm.jsx

--- resources/views/ProductForm.jsx
+++ resources/views/ProductForm.jsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button"
-import {Dialog,DialogContent,DialogDescription,DialogFooter,DialogHeader,DialogTitle,DialogTrigger,} from "@/components/ui/dialog"
+import {Dialog,DialogContent,DialogDescription,DialogFooter,DialogHeader,DialogTitle,} from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { toast } from 'sonner';
@@ -34,15 +34,13 @@ export function ProductForm({ open, setOpen, selectedProduct, data, setData, pos
             });
         }
     };
+    console.log('errors',errors);
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-{/*       <DialogTrigger asChild>
-        <Button variant="outline">Edit Pruduct</Button> 
-      </DialogTrigger>   */}
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>
-                        {selectedCity ? 'Edit Product' : 'Create New Product'}
+                        {selectedProduct ? 'Edit Product' : 'Create New Product'}
           </DialogTitle>
           <DialogDescription>
           </DialogDescription>
@@ -56,9 +54,9 @@ export function ProductForm({ open, setOpen, selectedProduct, data, setData, pos
             <Input id="name" value={data.name}
                 onChange={(e) => setData('name', e.target.value)}
                 onFocus={() => clearErrors('name')}
-                required
+                
                 className="col-span-3" />
-            {errors.name && <p className="mt-1 text-sm text-red-500">{errors.name}</p>}
+            {errors.name && <span className="mt-1 text-sm text-red-500 whitespace-nowrap">{errors.name}</span>}
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="description" className="text-right">
@@ -67,19 +65,19 @@ export function ProductForm({ open, setOpen, selectedProduct, data, setData, pos
             <Input id="description" value={data.description}
                 onChange={(e) => setData('description', e.target.value)}
                 onFocus={() => clearErrors('description')}
-                required
+                
                 className="col-span-3" />
-            {errors.description && <p className="mt-1 text-sm text-red-500">{errors.description}</p>}
+            {errors.description && <span className="mt-1 text-sm text-red-500 whitespace-nowrap">{errors.description}</span>}
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="price" className="text-right"
+            <Label htmlFor="price" className="text-right">
               price
             </Label>
             <Input id="price" value={data.price} className="col-span-3" 
                 onChange={(e) => setData('price', e.target.value)}
                 onFocus={() => clearErrors('price')}
-                required>/>
-            {errors.price && <p className="mt-1 text-sm text-red-500">{errors.price}</p>}
+                 />
+            {errors.price && <span className="mt-1 text-sm text-red-500 whitespace-nowrap">{errors.price}</span>}
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="stock" className="text-right">
@@ -88,8 +86,8 @@ export function ProductForm({ open, setOpen, selectedProduct, data, setData, pos
             <Input id="stock" value={data.stock} className="col-span-3" 
                 onChange={(e) => setData('stock', e.target.value)}
                 onFocus={() => clearErrors('stock')}
-                required>/>                        
-            {errors.stock && <p className="mt-1 text-sm text-red-500">{errors.stock}</p>}
+                 />                        
+            {errors.stock && <span className="mt-1 text-sm text-red-500 whitespace-nowrap">{errors.stock}</span>}
           </div>
         </div>
         <DialogFooter>
@@ -97,7 +95,7 @@ export function ProductForm({ open, setOpen, selectedProduct, data, setData, pos
                             Cancel
                         </Button>
                         <Button type="submit" disabled={processing}>
-                            {selectedCity ? 'Update':'Create'}
+                            {selectedProduct ? 'Update':'Create'}
                         </Button>
         </DialogFooter>
        </form>
