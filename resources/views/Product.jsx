8f4060b8ec706a5a612badbd10acc19a8be5776d
douha-c <dev204douha@gmail.com>
2025-05-07 05:47:38 +0200

    Create Product.jsx

---
+++ resources/views/Product.jsx
@@ -0,0 +1,112 @@
+import { DataTable } from '@/components/data-table';
+import { Button } from '@/components/ui/button';
+import { useDirection } from '@/context/DirectionProvider';
+import { useLang } from '@/hooks/Lang';
+import AppLayout from '@/layouts/admin-app-layout';
+import { Head, useForm } from '@inertiajs/react';
+import { Plus } from 'lucide-react';
+import { useState } from 'react';
+import { toast } from 'sonner';
+import { CitiesColumns } from './cities/cities-columns';
+import CityForm from './cities/city-form';
+import DeleteDialog from './cities/delete-dialog';
+
+export default function Cities({ cities }) {
+    const { __ } = useLang();
+    const [openDialog, setOpenDialog] = useState(false);
+    const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+    const [selectedCity, setSelectedCity] = useState(null);
+
+    const { direction } = useDirection();
+
+    const deleteMessage = __('The [:model] was successfully deleted', { model: __('city') });
+
+    const {
+        data,
+        setData,
+        post,
+        patch,
+        delete: destroy,
+        reset,
+        errors,
+        processing,
+        clearErrors,
+    } = useForm({
+        name: '',
+        name_ar: '',
+    });
+
+    const handleOpenForm = (city = null) => {
+        setSelectedCity(city);
+        setData({
+            name: city ? city.name : '',
+            name_ar: city ? city.name_ar : '',
+        });
+        clearErrors();
+        setOpenDialog(true);
+    };
+
+    const handleOpenDelete = (city) => {
+        setSelectedCity(city);
+        setOpenDeleteDialog(true);
+    };
+
+    const handleDelete = () => {
+        destroy(route('admin.city.delete', selectedCity.id), {
+            onSuccess: () => {
+                toast.success(deleteMessage);
+                setOpenDeleteDialog(false);
+            },
+        });
+    };
+
+    return (
+        <AppLayout header={__('Cities')}>
+            <Head title={__('Cities')} />
+            <div className="mx-auto mt-10 max-w-7xl">
+                <div className="mb-5 flex flex-col items-start justify-between gap-4 md:flex-row md:items-end">
+                    <h1 className="text-3xl font-bold">{__('Cities')}</h1>
+                    <Button onClick={() => handleOpenForm(null)}>
+                        <Plus className="mr-2 h-4 w-4" /> {__('Add :model', { model: __('city', {}, true) })}
+                    </Button>
+                </div>
+                <DataTable
+                    columns={CitiesColumns({
+                        handleOpenForm,
+                        handleOpenDelete,
+                        __,
+                    })}
+                    data={cities}
+                    filtering={{
+                        columns: ['name', 'name_ar'],
+                        placeholder: __('Search city...'),
+                    }}
+                    direction={direction}
+                />
+            </div>
+
+            <CityForm
+                open={openDialog}
+                setOpen={setOpenDialog}
+                selectedCity={selectedCity}
+                {...{
+                    data,
+                    setData,
+                    post,
+                    patch,
+                    reset,
+                    errors,
+                    processing,
+                    clearErrors,
+                }}
+            />
+            <DeleteDialog
+                open={openDeleteDialog}
+                setOpen={setOpenDeleteDialog}
+                handleDelete={handleDelete}
+                processing={processing}
+                direction={direction}
+            />
+        </AppLayout>
+    );
+}
